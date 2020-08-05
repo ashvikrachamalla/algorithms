a00e362a4444e12732ce6823c477df202c4b451b
ashvikrachamalla <68549074+ashvikrachamalla@users.noreply.github.com>
2020-08-05 17:27:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,24 +14,11 @@ box2.velocityY = 5;
 
 function draw() {
   background(255,255,255); 
-  if (box2.x - box1.x < box1.width/2 + box2.width/2
-    && box1.x - box2.x < box1.width/2 + box2.width/2
-    && box2.y - box1.y < box1.height/2 + box2.height/2
-    && box1.y - box2.y < box1.height/2 + box2.height/2){
+  if (isTouching(box1,box2)){
       box1.shapeColor = "black";
   box2.shapeColor = "blue";
    
     }
-    if (box2.x - box1.x < box1.width/2 + box2.width/2
-    && box1.x - box2.x < box1.width/2 + box2.width/2){
-      box1.velocityX = box1.velocityX * (-1);
-      box2.velocityX = box2.velocityX * (-1);
-      
-    }
-    if ( box2.y - box1.y < box1.height/2 + box2.height/2
-      && box1.y - box2.y < box1.height/2 + box2.height/2){
-        box1.velocityY = box1.velocityY * (-1);
-        box2.velocityY = box2.velocityY * (-1);  
-      }
+    
   drawSprites();
 }
